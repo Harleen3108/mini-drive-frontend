b9b9502542a9add7e68757d73dd628daec0240dc
Harleen3108 <151908029+Harleen3108@users.noreply.github.com>
2025-10-10 23:23:01 +0530

    Fix deployment structure

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@supabase/supabase-js'],
-  },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
